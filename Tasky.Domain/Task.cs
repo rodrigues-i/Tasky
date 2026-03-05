f518c6e1881ef13482e530e00dd192a9cecd5a44
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-03-05 00:09:12 -0300

    Create model Task

---
+++ Tasky.Domain/Task.cs
@@ -0,0 +1,20 @@
+﻿namespace Tasky.Domain
+{
+    public class Task
+    {
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public Guid? AssignedUserId { get; private set; }
+
+        public Task(Guid id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+
+        public void AssignUser(Guid userId)
+        {
+            AssignedUserId = userId;
+        }
+    }
+}
