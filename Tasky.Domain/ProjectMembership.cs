7592c412588c87dafe397bae66c9bc19d7e0fb5d
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-03-05 00:12:24 -0300

    Create model ProjectMemberShip

---
+++ Tasky.Domain/ProjectMembership.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasky.Domain
+{
+    public class ProjectMembership
+    {
+        public Guid UserId { get; private set; }
+
+        public ProjectMembership(Guid userId)
+        {
+            UserId = userId;
+        }
+    }
+}
