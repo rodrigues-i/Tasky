1d9a189e95836ea75e6049d2d255f8ec2e95959b
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-03-05 00:20:08 -0300

    Create model Project

---
+++ Tasky.Domain/Project.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasky.Domain
+{
+    public class Project
+    {
+        private readonly List<Task> _tasks = new List<Task>();
+        private readonly List<ProjectMembership> _memberships = new List<ProjectMembership>();
+
+        public IReadOnlyCollection<Task> Tasks => _tasks;
+        public IReadOnlyCollection<ProjectMembership> Memberships => _memberships;
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Project(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
