f8a17215b75ec41750b0a87092ffe36afeed9a80
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-03-07 14:00:50 -0300

    Create infrastructure layer and non-implemented repositories

---
+++ Tasky.Infrastructure/Persistence/Repositories/ProjectRepository.cs
@@ -0,0 +1,29 @@
+﻿using Tasky.Domain.Interfaces;
+using Tasky.Domain.projects;
+using Task = System.Threading.Tasks.Task;
+
+namespace Tasky.Infrastructure.Persistence.Repositories
+{
+    public class ProjectRepository : IProjectRepository
+    {
+        public Task<Project> GetProjectById(Guid projectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateProject(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProject(Guid projectId, Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProject(Guid projectId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tasky.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using Tasky.Domain.Entities;
+using Tasky.Domain.Interfaces;
+using Task = System.Threading.Tasks.Task;
+
+namespace Tasky.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<User> GetUserById(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUser(Guid userId, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
