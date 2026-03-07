5b7c653e1876e73a89f25d05ad1d2e4d9b2ec115
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-03-07 13:45:36 -0300

    Create repository interfaces

---
+++ src/Tasky.Domain/Interfaces/IProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using Tasky.Domain.projects;
+using Task = System.Threading.Tasks.Task;
+
+namespace Tasky.Domain.Interfaces
+{
+    public interface IProjectRepository
+    {
+        Task<Project> GetProjectById(Guid projectId);
+        Task CreateProject(Project project);
+        Task UpdateProject(Guid projectId, Project project);
+        Task DeleteProject(Guid projectId);
+    }
+}
---
+++ src/Tasky.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Tasky.Domain.Entities;
+using Task = System.Threading.Tasks.Task;
+namespace Tasky.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserById(Guid userId);
+        Task CreateUser(User user);
+        Task UpdateUser(Guid userId, User user);
+        Task DeleteUser(Guid userId);
+    }
+}
