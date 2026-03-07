88c09c1ffc34b4815e1a4e6bdb569719450d9e5d
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-03-07 13:08:37 -0300

    Create aggregate root user

---
+++ src/Tasky.Domain/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Tasky.Domain
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+
+        public User(Guid userId, string name)
+        {
+            Id = userId;
+            Name = name;
+        }
+    }
+}
