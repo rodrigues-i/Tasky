178c190bff6b16814352883fed53a97fca23f288
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-03-08 19:53:29 -0300

    Add packages for entity framework core and create db context

--- src/Tasky.Api/Program.cs
+++ src/Tasky.Api/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Tasky.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
---
+++ src/Tasky.Infrastructure/Persistence/AppDbConext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tasky.Domain.Entities;
+using Project = Tasky.Domain.Entities.Project;
+using Task = Tasky.Domain.Entities.Task;
+
+namespace Tasky.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectMembership> ProjectMemberships { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+    }
+}
