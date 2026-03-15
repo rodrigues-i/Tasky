d0180791970d709dcdba98b1f35ee0465d9d2e63
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-03-14 21:31:13 -0300

    Create interface for UserService

---
+++ src/Tasky.Application/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Tasky.Domain.Entities;
+using Task = System.Threading.Tasks.Task;
+
+namespace Tasky.Application.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<IEnumerable<User>> GetAllUsers();
+        public Task<User> GetUserById(Guid userId);
+        public Task CreateUser(User user);
+        public Task UpdateUser(User user);
+        public Task DeleteUser(Guid userId);
+    }
+}
