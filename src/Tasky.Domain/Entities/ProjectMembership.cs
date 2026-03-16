7bd3be3d024230b6e60bcecf7761446ea451d1cb
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-03-15 22:42:31 -0300

    Add proejectId to help creating composite key

--- src/Tasky.Domain/Entities/ProjectMembership.cs
+++ src/Tasky.Domain/Entities/ProjectMembership.cs
@@ -8,10 +8,12 @@ namespace Tasky.Domain.Entities
 {
     public class ProjectMembership
     {
+        public Guid ProjectId { get; private set; }
         public Guid UserId { get; private set; }
 
-        public ProjectMembership(Guid userId)
+        public ProjectMembership(Guid projectId, Guid userId)
         {
+            ProjectId = projectId;
             UserId = userId;
         }
     }
