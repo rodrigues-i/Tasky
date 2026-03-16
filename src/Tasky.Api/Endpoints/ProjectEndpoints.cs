82f7aab0a680613216450e7c6931921f9639bbb1
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-03-15 22:13:53 -0300

    Create minimal api endpoint

---
+++ src/Tasky.Api/Endpoints/ProjectEndpoints.cs
@@ -0,0 +1,24 @@
+﻿using Tasky.Domain.Entities;
+
+namespace Tasky.Api.Endpoints
+{
+    public static class ProjectEndpoints
+    {
+        public static void MapProjectEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapGet("/projects", () =>
+            {
+                var user1 = new User(Guid.NewGuid(), "Lucas");
+                var user2 = new User(Guid.NewGuid(), "João");
+                var user3 = new User(Guid.NewGuid(), "Pedro");
+                var user4 = new User(Guid.NewGuid(), "Mateus");
+
+                var users = new List<User> { user1, user2, user3, user4 };
+
+                return Results.Ok(users);
+            })
+                .WithName("GetUsers")
+                .WithOpenApi();
+        }
+    }
+}
--- src/Tasky.Api/Program.cs
+++ src/Tasky.Api/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Tasky.Api.Endpoints;
 using Tasky.Application.Interfaces;
 using Tasky.Application.Services;
+using Tasky.Domain.Entities;
 using Tasky.Domain.Interfaces;
 using Tasky.Infrastructure.Persistence;
 using Tasky.Infrastructure.Persistence.Repositories;
@@ -31,29 +33,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+
+app.MapProjectEndpoints();
+//app.MapUerEndpoints();
+
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
