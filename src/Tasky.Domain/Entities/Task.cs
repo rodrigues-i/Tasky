a884012159e0ea013cedc3cc1baaa3ee05395db2
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-03-22 11:01:10 -0300

    Add properties projectId and Projects to the model Task

--- src/Tasky.Domain/Entities/Task.cs
+++ src/Tasky.Domain/Entities/Task.cs
@@ -5,11 +5,14 @@
         public Guid Id { get; private set; }
         public string Title { get; private set; }
         public Guid? AssignedUserId { get; private set; }
+        public Guid ProjectId { get; private set; }
+        public Project? Project { get; private set; }
 
-        public Task(Guid id, string title)
+        public Task(Guid id, string title, Guid projectId)
         {
             Id = id;
             Title = title;
+            ProjectId = projectId;
         }
 
         public void AssignUser(Guid userId)
