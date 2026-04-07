fa0fe3687c134a731efe6efe201cf698605eebd8
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-04-06 23:31:57 -0300

    Craeate role attribute in user entity

--- src/Tasky.Domain/Entities/User.cs
+++ src/Tasky.Domain/Entities/User.cs
@@ -7,16 +7,18 @@
         public string Email { get; private set; }
         public byte[] PasswordHash { get; private set; }
         public byte[] PasswordSalt { get; private set; }
+        public string Role { get; private set; } = "User";
 
         public User() { }
 
-        public User(Guid id, string name, string email, byte[] hash, byte[] salt)
+        public User(Guid id, string name, string email, byte[] hash, byte[] salt, string role = "User")
         {
             Id = id;
             Name = name;
             Email = email;
             PasswordHash = hash;
             PasswordSalt = salt;
+            Role = role;
         }
 
         public void UpdateUserDetails(User user)
